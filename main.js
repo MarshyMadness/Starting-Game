e855083f2316f35838626bd1a5a9cefd84423e85
MarshyMadness <102564915+MarshyMadness@users.noreply.github.com>
2022-04-04 20:13:03 -0600

    Bug Fixes, Proper Save Game, UI Changes
    Un-broke the game, changed the UI and fixed the save game functionality as well as clear save

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var gameData = {
 	GameVersion: "v0.0.4",
+	foodAmount: 0,
+	foodPerClick: 1,
+	foodPerClickCost: 50,
 	wood: 0,
 	woodPerClick: 1,
 	woodPerClickCost: 50,
@@ -12,16 +15,133 @@ var gameData = {
 	bronzePerClickUpgradeNum: 0
 };
 
+var SAVE_KEY = gameData;
+
+/*$(function() {
+            $("#GatherFoodProgressBar").progressbar({
+                value: 90
+  
+            });
+        });
+*/
+
+function FixBorder(){
+	$("#Buildings").parent("div").hide();
+}
 $(document).ready(function() {
     $(".ResourceButtons").click(function(){
     });
 });
 
 $(document).ready(function() {
-    $("#People").click(function openTabs2(TabName){
-    TabName = "People";
-	openTab("People");	
 	
+	$("#gatherFoodButton").click(function(){
+		gatherFood();
+		
+		
+		/*
+		var currValue = $( "#gatherFoodButton" ).data("value");
+        currValue = parseInt(currValue) ? parseInt(currValue) : 0;
+        if(currValue <= 100) {
+            $( "#gatherFoodButton" ).progressbar({
+              value: currValue+1
+            }).data("value",currValue+1);
+            $("#gatherFoodButton").html("Gather Food " + (currValue+1)+"%");
+        }  
+		*/
+		
+		
+		/*
+		var progress = 0;
+
+		//change button to progress button, and add progress bar
+		$('#gatherFoodButton').addClass('progress').html('<span id="gatherFoodButton"></span>');
+
+		//update progress bar every 0.5 second
+		setInterval(function(){
+			$('#gatherFoodButton').width(progress);
+			progress++;
+			document.getElementsById('gatherFood').innerHTML = "Gather food " + progress + "%"
+		}, 10);
+		*/
+		  $( function() {
+			var progressbar = $( "#gatherFoodProgressBar" ),
+			  progressLabel = $( "#gatherFoodButtonLabel" );
+		 
+			progressbar.progressbar({
+			  value: false,
+			  change: function() {
+				progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+			  },
+			  complete: function() {
+				progressLabel.text( "Complete!" );
+			  }
+			});
+		 
+			function progress() {
+			  var val = progressbar.progressbar( "value" ) || 0;
+		 
+			  progressbar.progressbar( "value", val + 2 );
+		 
+			  if ( val < 99 ) {
+				setTimeout( progress, 80 );
+			  }
+			  if (val == 100) {
+				  
+			  }
+			}
+			
+		  } );
+				
+		
+		
+
+	});
+	$("#cutWoodButton").click(function(){ cutWood(); });
+	$("#mineCopperButton").click(function(){ minecopper(); });
+	
+	$(".close").click(function(){
+		//onclick="this.parentElement.style.display='none'"
+		$("#Buildings").parent("div").hide();
+		$("#People").parent("div").hide();
+	});
+	
+    $("#Peoplebutton").click(function(){
+	var i, tabcontent, tablinks;
+
+	  // Get all elements with class="tabcontent" and hide them
+	  tabcontent = document.getElementsByClassName("tabcontent");
+	  for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].classList.add("Hidden"); //.display = "none";
+		tabcontent[i].classList.remove("visibleblock");
+	  }
+
+	    $("#People").parent("div").show();
+		$('#People').addClass('visibleblock');
+		$('#Peoplebutton').addClass('active');
+		$('#Peoplebutton').removeClass('Hidden');
+	});
+	
+	$("#Buildingsbutton").click(function(){
+	var i, tabcontent, tablinks;
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].classList.add("Hidden"); //.display = "none";
+		tabcontent[i].classList.remove("visibleblock");
+	}
+		$("#Buildings").parent("div").show();
+		$('#Buildings').addClass('visibleblock');
+		$('#Buildingsbutton').addClass('active');
+		$('#Buildingsbutton').removeClass('Hidden');
+		
+	});	
+	
+	
+	
+	
+	$("#SaveButton").click(function(){
+		save(gameData);
 	});
 });
 
@@ -29,33 +149,43 @@ $(document).ready(function() {
 function openTab(TabNames) {
 	  // Declare all variables
 	  var i, tabcontent, tablinks;
+	  console.log(TabNames);
 
 	  // Get all elements with class="tabcontent" and hide them
 	  tabcontent = document.getElementsByClassName("tabcontent");
 	  for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
+		tabcontent[i].classList.add("Hidden"); //.display = "none";
+		tabcontent[i].classList.remove("visibleblock");
 	  }
 
 	  // Get all elements with class="tablinks" and remove the class "active"
-	  tablinks = document.getElementsByClassName("tablinks");
-	  for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	  }
+	  //tablinks = document.getElementsByClassName("tablinks");
+	  //for (i = 0; i < tablinks.length; i++) {
+		//tablinks[i].className = tablinks[i].className.replace(" active", "");
+	  //}
 
 	  // Show the current tab, and add an "active" class to the button that opened the tab
 		//document.getElementById(TabName).setAttribute("style", "display: block;"); //.style.display = "block");
 		$(TabNames).addClass('Visibleblock');
+		$(TabNames).removeClass('Hidden');
 		//document.getElementById(TabName).classList.add('active');
-		TabNames.className += " active";
+		//TabNames.className += " active";
 
 		//var element = document.getElementById("defaultOpen");
 		//element.classList.add("dblock");
 		//element.classList.remove("dnone");
 } 
+
+function gatherFood() {
+	  gameData.foodAmount += gameData.foodPerClick
+	  document.getElementById("foodGathered").innerHTML = gameData.foodAmount
+	  document.getElementById("foodPerClickUpgrade").innerHTML = "Upgrade Hatchet (Currently Level " + gameData.foodPerClick + ") Cost: " + gameData.foodPerClickCost + " copper"
+}
+
 function cutWood() {
 	  gameData.wood += gameData.woodPerClick
 	  document.getElementById("woodCut").innerHTML = gameData.wood
-	  document.getElementById("copperperClickUpgrade").innerHTML = "Upgrade Copper Pickaxe (Currently Level " + gameData.copperPerClick + ") Cost: " + gameData.copperPerClickCost + " copper"
+	  document.getElementById("woodPerClickUpgrade").innerHTML = "Upgrade Hatchet (Currently Level " + gameData.copperPerClick + ") Cost: " + gameData.copperPerClickCost + " copper"
 }
 
 function minecopper() {
@@ -83,10 +213,21 @@ function minebronze() {
 	  }
 	}
  })
+ 
 var mainGameLoop = window.setInterval(function() {
-  //minecopper(); cutWood();
+  //minecopper(); cutWood(); gatherFood();
+	document.getElementById("foodGathered").innerHTML = gameData.foodAmount
+  
   	document.getElementById("gameVersion").innerHTML = "Version " + gameData.GameVersion
-	document.getElementById("copperMinedPerSecond").innerHTML = gameData.copperPerClick
+	//document.getElementById("copperMinedPerSecond").innerHTML = gameData.copperPerClick
+  
+  
+	if(gameData.foodAmount >= 50){
+	  document.getElementById("foodPerClickUpgrade").style.display="inline-block";
+	}
+   if(gameData.wood >= 50){
+	  document.getElementById("woodPerClickUpgrade").style.display="inline-block";
+  }
   
   if(gameData.copper >= 50){
 	  document.getElementById("copperperClickUpgrade").style.display="inline-block";
@@ -109,6 +250,9 @@ var mainGameLoop = window.setInterval(function() {
   })
 }, 1000)
 
+
+
+
 var savegame = JSON.parse(localStorage.getItem("copperMinerSave"))
 if (savegame !== null) {
   gameData = savegame
@@ -118,24 +262,26 @@ var saveGameLoop = window.setInterval(function() {
   localStorage.setItem('copperMinerSave', JSON.stringify(gameData))
 }, 15000)
 
-function save() {
-    let dec = decimalToString(game);
-    let str = JSON.stringify(dec);
-    localStorage.setItem("Factorygamesave", str);
-	document.getElementById("Save").style.justifyContent = "center";
+function save(gameData) {
+   // let dec = decimalToString(game);
+    //let str = JSON.stringify(dec);
+    //localStorage.setItem("Factorygamesave", str);
+	//document.getElementById("Save").style.justifyContent = "center";
+	window.localStorage.setItem(SAVE_KEY, JSON.stringify(gameData));
 }
 
 function load() {
-    let str = localStorage.getItem("Factorygamesave");
-    if (str == undefined || str == "undefined" || str == null) return;
-    let sav = stringToDecimal(JSON.parse(str));
+    //let str = localStorage.getItem("Factorygamesave");
+    //if (str == undefined || str == "undefined" || str == null) return;
+    //let sav = stringToDecimal(JSON.parse(str));
+	gameData = JSON.parse(window.localStorage.getItem(SAVE_KEY));
 }
 
 function ClearSave() {
 	localStorage.clear();
 	window.location.reload();
-    localStorage.setItem("Factorygamesave", "0");
-	let str = localStorage.getItem("Factorygamesave");
-    if (str == undefined || str == "undefined" || str == null) return;
-	let sav = stringToDecimal(JSON.parse(str));
+    //localStorage.setItem("Factorygamesave", "0");
+	//let str = localStorage.getItem("Factorygamesave");
+    //if (str == undefined || str == "undefined" || str == null) return;
+	//let sav = stringToDecimal(JSON.parse(str));
 }
