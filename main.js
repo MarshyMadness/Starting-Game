e6479d264649f5d1a1ef844c3c54e77dc3413abb
MarshyMadness <102564915+MarshyMadness@users.noreply.github.com>
2022-04-05 21:02:36 -0600

    Not sure

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+	
 	var gameData = {
 		GameVersion: "v0.0.4",
 		foodAmount: 0,
@@ -15,6 +17,8 @@
 		bronzePerClickUpgradeNum: 0,
 		TotalTime: 0
 	};
+	
+	var Time = gameData.TotalTime;
 
 	var SAVE_KEY = gameData;
 
@@ -27,24 +31,115 @@
 	*/
 
 	function FixBorder(){
-		$("#Buildings").parent("div").show();
-		$('#Buildings').addClass('visibleblock');
-		$('#Buildingsbutton').addClass('active');
-		$('#Buildingsbutton').removeClass('Hidden');
+		//$("#Buildings").parent("div").show();
+		//$('#Buildings').addClass('visibleblock');
+		//$('#Buildingsbutton').addClass('active');
+		//$('#Buildingsbutton').removeClass('Hidden');
 		$("#Buildings").parent("div").hide();
 		loop.start();
 	}
+	
+	$(document).ready(function() {
+		
+		$( "#GatherFoodprogress_bar" ).progressbar({
+			value: 0,
+			max:100,
+			min:0
+		});
+	});
+	
 	$(document).ready(function() {
 		$(".ResourceButtons").click(function(){
+		});
+		
+		$( function() {
+
+			$( "#OptionsDialog" ).dialog({
+				autoOpen: false,
+				show: {
+					effect: "blind",
+					duration: 1000
+				},
+				hide: {
+					effect: "blind",
+					duration: 1000
+				},
+				width: 500,
+				title: "Game Stats"
+			});
+		
+		});
+			
+		$( "#MenuIcon" ).on( "click", function() {
+		  $( "#OptionsDialog" ).dialog( "open" );
+		});
+		
+		$( function() {
+
+			$( "#GatherDialog" ).dialog({
+				autoOpen: false,
+				show: {
+					effect: "blind",
+					duration: 1000
+				},
+				hide: {
+					effect: "blind",
+					duration: 1000
+				},
+				width: "auto",
+				height: "auto",
+				position: { 
+				my: "center top", at: "center top", of: window
+				},
+				title: "Gather",
+				resizable: false
+			});
+		
 		});
 	});
 
 	$(document).ready(function() {
-		
-		$("#gatherFoodButton").click(function(){
-			//$("#foodprogressBar").parent("div").css( "height", "2rem");
-			//gatherFood();
+		$("#start").click(function(){
+			if (loop.isPaused) {
+				loop.resume();
+			}
 			
+			if (loop.isStopped) {
+				loop.start();
+			}
+		
+		});
+		
+		$("#MenuIcon").click(function(){
+		
+		});
+		
+		$("#pause").click(function(){
+			loop.pause();
+		});
+	
+		/*
+		$("#gatherFoodButton").click(function(){
+			let x=0;
+			
+			
+			while (x<=100){
+				$( "#GatherFoodprogress_bar" ).progressbar({
+					value: x
+				});
+				document.getElementById("gatherFoodButton").disabled = true;
+				x+= 1;
+			};
+			document.getElementById("gatherFoodButton").disabled = false;
+		*/
+			
+			
+			
+			
+			
+			
+			
+		/*
 			var progressBar = $('#foodprogressBar');
 			var elem = document.getElementById('foodprogressBar');
 			var positionInfo = elem.getBoundingClientRect();
@@ -80,18 +175,18 @@
 					//width = $('#foodprogressBar').progressbar("option","value");
 					//width += 50;
 					
-					/*$( "#foodprogressBar" ).progressbar({
+					$( "#foodprogressBar" ).progressbar({
 					  change: function( event, ui ) {
 						  width = $('#foodprogressBar').progressbar("option","value");
 					  }
 					});
 					elem.style.width = width + '%'; 
 					elem.innerHTML = width * 1  + '%';
-					*/
+					
 				}
 			}, 2100);
 			document.getElementById("gatherFoodButton").disabled = false;
-			
+		*/
 			
 			
 			/*
@@ -151,10 +246,6 @@
 			  } );
 			  */
 					
-			
-			
-
-		});
 		$("#cutWoodButton").click(function(){ cutWood(); });
 		$("#mineCopperButton").click(function(){ minecopper(); });
 		
@@ -162,19 +253,44 @@
 			//onclick="this.parentElement.style.display='none'"
 			$("#Buildings").parent("div").hide();
 			$("#People").parent("div").hide();
+			$('#Buildingsbutton').removeClass('active');
+			$('#People').removeClass('active');
+		});
+		
+		$("#Gatherbutton").on( "click", function(){			
+			$( "#GatherDialog" ).dialog( "open" );
 		});
 		
 		$("#Peoplebutton").click(function(){
-		var index, tabcontent, tablinks;
+		//var index, tabcontent, tablinks;
 
 		  // Get all elements with class="tabcontent" and hide them
+		  //tabcontent = document.getElementsByClassName("tabcontent");
 		  tabcontent = document.getElementsByClassName("tabcontent");
+		  //tabcontent = Array.from(tabcontent);
+		 // console.log(tabcontent);
+		  //console.log(tabcontent.length);
+		  //tabcontent = $("#tabcontent").prop("classList");
+		  //tabcontent = tabcontent.classList;
 		  
+		  /*
 		  for (index = 0; index < tabcontent.length; index++) {
 			tabcontent[index].classList.add("Hidden"); //.display = "none	";
 			tabcontent[index].classList.remove("visibleblock");
 			tabcontent[index].classList.remove('active');
-		  }
+			$(tabcontent[index]).addClass('Hidden');
+			$(tabcontent[index]).removeClass('Active');
+			}
+			*/
+			
+			var elems = document.querySelectorAll(".active");
+				[].forEach.call(elems, function(el) {
+				el.classList.remove("active");
+				[].forEach.call(tabcontent, function(el) {
+				el.classList.add("Hidden");
+			});
+			  });
+		  
 
 			$("#People").parent("div").show();
 			$('#People').addClass('visibleblock');
@@ -183,60 +299,26 @@
 		});
 		
 		$("#Buildingsbutton").click(function(){
-		var i, tabcontent, tablinks;
-		// Get all elements with class="tabcontent" and hide them
-		tabcontent = document.getElementsByClassName("tabcontent");
-		for (i = 0; i < tabcontent.length; i++) {
-			tabcontent[i].classList.add("Hidden"); //.display = "none";
-			tabcontent[i].classList.remove("visibleblock");
-			tabcontent[i].classList.remove('active');	
-		}
-			$("#Buildings").parent("div").show();
-			$('#Buildings').addClass('visibleblock');
-			$('#Buildingsbutton').addClass('active');
-			$('#Buildingsbutton').removeClass('Hidden');
+				var elems = document.querySelectorAll(".active");
+					[].forEach.call(elems, function(el) {
+					el.classList.remove("active");
+					[].forEach.call(tabcontent, function(el) {
+					el.classList.add("Hidden");
+					});
+					});
+				
+				$("#Buildings").parent("div").show();
+				$('#Buildings').addClass('visibleblock');
+				$('#Buildingsbutton').addClass('active');
+				$('#Buildingsbutton').removeClass('Hidden');
 			
-		});	
 		
-		
-		
-		
+		}); 
+			
 		$("#SaveButton").click(function(){
 			save(gameData);
 		});
 	});
-
-
-	function openTab(TabNames) {
-		  // Declare all variables
-		  var i, tabcontent, tablinks;
-		  console.log(TabNames);
-
-		  // Get all elements with class="tabcontent" and hide them
-		  tabcontent = document.getElementsByClassName("tabcontent");
-		  for (i = 0; i < tabcontent.length; i++) {
-			tabcontent[i].classList.add('Hidden'); //.display = "none";
-			tabcontent[i].classList.remove("visibleblock");
-		  }
-
-		  // Get all elements with class="tablinks" and remove the class "active"
-		  //tablinks = document.getElementsByClassName("tablinks");
-		  //for (i = 0; i < tablinks.length; i++) {
-			//tablinks[i].className = tablinks[i].className.replace(" active", "");
-		  //}
-
-		  // Show the current tab, and add an "active" class to the button that opened the tab
-			//document.getElementById(TabName).setAttribute("style", "display: block;"); //.style.display = "block");
-			$(TabNames).addClass('Visibleblock');
-			$(TabNames).removeClass('Hidden');
-			//document.getElementById(TabName).classList.add('active');
-			//TabNames.className += " active";
-
-			//var element = document.getElementById("defaultOpen");
-			//element.classList.add("dblock");
-			//element.classList.remove("dnone");
-	} 
-
 	function gatherFood() {
 		  gameData.foodAmount += gameData.foodPerClick
 		  document.getElementById("foodGathered").innerHTML = gameData.foodAmount
@@ -261,7 +343,7 @@
 		  document.getElementById("bronzeperClickUpgrade").innerHTML = "Upgrade Bronze Pickaxe (Currently Level " + gameData.copperPerClick + ") Cost: " + gameData.copperPerClickCost + " copper"
 	}
 
-	 $( document ).ready(function(){
+	$( document ).ready(function(){
 		function buycopperPerClick() {
 			document.getElementById("copperMined").innerHTML = gameData.copper
 			document.getElementById("copperperClickUpgrade").innerHTML = "Upgrade Copper Pickaxe (Currently Level " + gameData.copperPerClick + ") Cost: " + gameData.copperPerClickCost + " copper"
@@ -273,7 +355,7 @@
 			gameData.copperPerClickCost *= 2
 		  }
 		}
-	 })
+	})
 	 
 	var mainGameLoop = window.setInterval(function() {
 	  //minecopper(); cutWood(); gatherFood();
@@ -314,11 +396,6 @@
 
 
 
-	var savegame = JSON.parse(localStorage.getItem("copperMinerSave"))
-	if (savegame !== null) {
-	  gameData = savegame
-	}
-
 	var saveGameLoop = window.setInterval(function() {
 	  localStorage.setItem('copperMinerSave', JSON.stringify(gameData))
 	}, 15000)
@@ -345,24 +422,7 @@
 		//let str = localStorage.getItem("Factorygamesave");
 		//if (str == undefined || str == "undefined" || str == null) return;
 		//let sav = stringToDecimal(JSON.parse(str));
-	}
-	
-	
-	
-	$(function() {
-		$( "#foodprogressBar").progressbar({
-		value: 00,
-		max:100
-		});
-		var pb = $( "#foodprogressBar" );
-		function progress() {
-		var v = pb.progressbar( "value" ) || 0;
-		pb.progressbar( "value", v + 1 );
-		
-		}
-	});
-	
-	
+	}	
 	
 	const STOPPED = Symbol.for("@@gameloop/stopped");
 	const PAUSED = Symbol.for("@@gameloop/paused");
@@ -399,6 +459,7 @@
 		  
 		  this.timing = { last, total, delta, lag };
 		  this.frame = requestAnimationFrame(this.tick);
+		  Time = gameData.TotalTime;
 		}
 	  }
 	  stop() {
@@ -451,9 +512,11 @@
 
 	loop.onUpdate = function(dt, t) {
 	  //currency += currency_per_millisecond * dt;
-	  document.getElementById("Time").innerHTML = "Time Elapsed (Seconds): " + Math.round(t/60);
-	  gameData.TotalTime = t;
+	  document.getElementById("Time").innerHTML = "Current Playtime: " + "<span class=\"TimeColor\">" + Math.round(t/1000)+" seconds</span>";
+	  gameData.TotalTime += dt;
+	  document.getElementById("TimeElapsedTotal").innerHTML = "Total Time Played: " + "<span class=\"TimeColor\">" + (Math.round(gameData.TotalTime/60000) + " minutes</span/>");
 	};
+	
 
 	loop.onRender = function(i) {
 	  //currencyEl.textContent = currency.toFixed(2);
